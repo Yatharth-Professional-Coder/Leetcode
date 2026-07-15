cdcf5aa28ac607cdf0c97242685a16e0ec2d6824
Yatharth <yatharth.madaan23@lpu.in>
2026-07-15 10:40:35 +0530

    Time: 39 ms (77.96%), Space: 82.8 MB (78.53%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,15 +1,28 @@
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         unordered_set<int> s;
+//         for(int num:nums){
+//             if(s.count(num)){
+//                 return true;
+
+//             }
+//             s.insert(num);
+//         }
+//         return false;
+
+//     }
+// };
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> s;
-        for(int num:nums){
-            if(s.count(num)){
-                return true;
-
-            }
-            s.insert(num);
+        // sorting array
+        // for any same return true
+        sort(nums.begin(),nums.end());
+        if(nums.size()==1) return false;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]) return true;
         }
         return false;
-
     }
 };
