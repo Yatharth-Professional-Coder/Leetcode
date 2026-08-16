16952856e70ce90fee79c6ce0c4b56c58bee6ad4
Yatharth <yatharth.madaan23@lpu.in>
2026-08-16 08:37:17 +0530

    Time: 0 ms (100%), Space: 22.4 MB (99.96%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -4,8 +4,7 @@ public:
         int k=0;
         for(int i=1;i<nums.size();i++){
             if(nums[i]!=nums[k]){
-                k++;
-                nums[k]=nums[i];
+                nums[++k]=nums[i];
             }
         }
         return k+1;
