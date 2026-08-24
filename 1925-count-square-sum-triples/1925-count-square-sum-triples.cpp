9740815cf400b9677ee01cb4218554f601f2e6de
Yatharth <yatharth.madaan23@lpu.in>
2026-08-24 05:38:31 +0530

    Time: 10 ms (44.65%), Space: 8.3 MB (6.67%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int countTriples(int n) {
-        int cnt = 0;
-        for (int a = 3; a < n; a++){
-            int sqrt_a = a * a;
-            for (int b = 3; b < n; b++){
-                int sqrt_sum = sqrt_a + b * b;
-                int c = (int)std::sqrt(sqrt_sum);
-                if (c > n)
-                    break;
-                if (c * c == sqrt_sum){
-                    cnt++;
+        vector<int>square;
+        for (int i = 1; i <= n; i++) {
+            square.push_back(i * i);
+        }
+        int count = 0;
+        for (int a = 0; a < n; a++) {
+            for (int b = 0; b < n; b++) {
+                int sum = square[a] + square[b];
+                int c = sqrt(sum);
+                if (c <= n && c * c == sum) {
+                    count++;
                 }
             }
         }
-        return cnt;
+        return count;
     }
 };
